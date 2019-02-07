c379e24bb40298ced8696838cb1e97393502b256
Дмитрий <yellow@MacBook-Air-Dmitrij.local>
2019-02-07 21:45:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dima/webbiewtest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.dima.webbiewtest;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView browser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String s="file:///android_res/raw/index.html";
+
+        browser = findViewById(R.id.webview);
+
+        browser.setWebViewClient(new MyWebViewClient());
+        browser.loadUrl(s);
+
+        browser.getSettings().setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(browser.canGoBack()) {
+            browser.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
+
+        // Для старых устройств
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+}
